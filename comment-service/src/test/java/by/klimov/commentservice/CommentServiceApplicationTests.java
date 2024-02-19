dfebae600b3b3c02dcae372006b0f3407271b6c6
Dmitry Klimov <dima.klimov.98@inbox.ru>
2024-02-19 21:08:32 +0300

    generate project

---
+++ comment-service/src/test/java/by/klimov/commentservice/CommentServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package by.klimov.commentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentServiceApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
