ebc88a679441c17cf7ae14ff6ab64935bfffa4b5
Dmitry Klimov <dima.klimov.98@inbox.ru>
2024-02-28 21:45:16 +0300

    LoggingAspect api doc

--- comment-service/README.md
+++ comment-service/README.md
@@ -1,2 +1,16 @@
+# Comment service
+
+## Description
+Сервис для работы с комментариями
+
+### DOC
+JSON format:<br/>
+http://localhost:8081/v1/v3/api-docs<br/>
+YAML format:<br/>
+http://localhost:8081/v1/v3/api-docs.yaml<br/>
+SWAGGER:<br/>
+http://localhost:8081/v1/swagger-ui/index.html<br/>
+
+
 #### docker build . -t comment-service:1.0.0
 #### docker-compose up
--- comment-service/src/main/java/by/klimov/commentservice/CommentServiceApplication.java
+++ comment-service/src/main/java/by/klimov/commentservice/CommentServiceApplication.java
@@ -1,13 +1,16 @@
 package by.klimov.commentservice;
 
 import by.klimov.commentservice.lucene.Indexer;
+import by.klimov.commentservice.repository.impl.SearchRepositoryImpl;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(repositoryBaseClass = SearchRepositoryImpl.class)
 public class CommentServiceApplication {
 
   public static void main(String[] args) {
