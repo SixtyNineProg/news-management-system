c02d230809065f958cf1f6007c2e143120d43d5a
Dmitry Klimov <dima.klimov.98@inbox.ru>
2024-03-02 00:40:23 +0300

    news-service init commit

---
+++ news-service/src/test/java/by/klimov/newsservice/NewsServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package by.klimov.newsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsServiceApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
