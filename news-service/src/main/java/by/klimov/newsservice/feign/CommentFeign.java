115283896f0092bcacd380d0126c20cc11b9bfef
Dmitry Klimov <dima.klimov.98@inbox.ru>
2024-03-03 23:25:34 +0300

    synchronize ports

--- news-service/src/main/java/by/klimov/newsservice/feign/CommentFeign.java
+++ news-service/src/main/java/by/klimov/newsservice/feign/CommentFeign.java
@@ -12,10 +12,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "comment-service", url = "localhost:8081/v1/comments")
+@FeignClient(name = "comment-service", url = "http://comments-service:8080")
 public interface CommentFeign {
 
-  @PostMapping(value = "/filter", produces = MediaType.APPLICATION_JSON_VALUE)
+  @PostMapping(value = "/v1/comments/filter", produces = MediaType.APPLICATION_JSON_VALUE)
   ResponseEntity<Page<CommentDto>> getAllWithFilter(
       @RequestParam(name = "page_number") Integer pageNumber,
       @RequestParam(name = "page_size", defaultValue = "15", required = false) Integer pageSize,
