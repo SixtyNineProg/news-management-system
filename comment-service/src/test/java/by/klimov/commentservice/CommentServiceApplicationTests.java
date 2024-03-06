4befa8d9ed33a3cca6176ae72ce61d2dd920a3a3
Dmitry Klimov <dima.klimov.98@inbox.ru>
2024-03-07 00:11:00 +0300

    postgres sql container for tests

--- comment-service/src/test/java/by/klimov/commentservice/CommentServiceApplicationTests.java
+++ comment-service/src/test/java/by/klimov/commentservice/CommentServiceApplicationTests.java
@@ -1,10 +1,11 @@
 package by.klimov.commentservice;
 
+import by.klimov.commentservice.container.PostgresSqlContainerInitializer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CommentServiceApplicationTests {
+class CommentServiceApplicationTests extends PostgresSqlContainerInitializer {
 
   @Test
   void contextLoads() {}
--- news-service/src/test/java/by/klimov/newsservice/NewsServiceApplicationTests.java
+++ news-service/src/test/java/by/klimov/newsservice/NewsServiceApplicationTests.java
@@ -1,10 +1,11 @@
 package by.klimov.newsservice;
 
+import by.klimov.newsservice.container.PostgresSqlContainerInitializer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class NewsServiceApplicationTests {
+class NewsServiceApplicationTests extends PostgresSqlContainerInitializer {
 
   @Test
   void contextLoads() {}
